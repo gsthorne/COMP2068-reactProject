31b98f4a4fed04877160e5030cef3fe5fa224a5a
gsthorne <gsthorne@lakeheadu.ca>
2021-02-15 14:47:01 -0500

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Header from './shared/Header';
+
+const About = () => {
+   return (
+       <Header title="About" />
+   );
+}
+ 
+export default About;
---
+++ src/components/shared/Navigation.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Navigation = () => {
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/about">About</Link>
+                </li>
+            </ul>
+        </nav>
+    );
+}
+ 
+export default Navigation;
